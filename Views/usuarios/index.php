6963de5396abe67f3a119f11131336b9442b10f7
Aaron <dm319636@gmail.com>
2025-08-31 14:34:39 -0400

    segunda parte dos

--- Views/usuarios/index.php
+++ Views/usuarios/index.php
@@ -1,30 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuarios</title>
-</head>
-<body>
-    <h1>Lista de usuarios</h1>
+<?php require_once "Views/Templates/header.php"; ?>
 
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nick</th>
-                <th>Nombre</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($data['usuarios'] as $usuario): ?>
+<div class="card">
+    <div class="card-header">
+        <div class="card-title">Lista de Usuarios</div>
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
                 <tr>
-                    <td><?php echo $usuario['id_usuario']; ?></td>
-                    <td><?php echo $usuario['nick']; ?></td>
-                    <td><?php echo $usuario['nombre']; ?></td>
+                    <th>ID</th>
+                    <th>Nick</th>
+                    <th>Nombre</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($data['usuarios'] as $usuario): ?>
+                    <tr>
+                        <td><?php echo $usuario['id_usuario']; ?></td>
+                        <td><?php echo $usuario['nick']; ?></td>
+                        <td><?php echo $usuario['nombre']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
 </body>
+
 </html>
+
+<?php require_once "Views/Templates/footer.php"; ?>
